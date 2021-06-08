a0756e03d051c5f4de2f36cbb6c878ae2ecc78a8
Ribie14 <v.ciriane@gmail.com>
2021-06-08 10:14:32 +0200

    Jusqu'a 31:50 de la video 1 blog par steam

--- src/Controller/SteamController.php
+++ src/Controller/SteamController.php
@@ -8,11 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SteamController extends AbstractController
 {
-    #[Route('/steam', name: 'steam')]
+    #[Route('/jeux', name: 'jeux')]
     public function index(): Response
     {
         return $this->render('steam/index.html.twig', [
             'controller_name' => 'SteamController',
         ]);
+    }
+
+     //Permet de définir la page daccueil, la page basique du site
+
+    #[Route('/', name: 'home')]
+
+    public function home() {
+        return $this->render('steam/home.html.twig');
+    }
+
+    #[Route('/jeux/12', name: 'jeux_show')]
+
+    public function profile() {
+        return $this->render('steam/show.html.twig');
     }
 }
