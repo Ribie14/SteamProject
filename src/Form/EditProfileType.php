35656afc65903114c5f4a7304a2e2c52a72dfda2
Ribie14 <v.ciriane@gmail.com>
2021-06-23 14:42:56 +0200

    Modifier

--- src/Form/EditProfileType.php
+++ src/Form/EditProfileType.php
@@ -6,13 +6,14 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EditProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TexteType::class)
+            ->add('username')
             ->add('Valider', SubmitType::class)
         ;
     }
